bf7fa3f21ebb60d5fc0be0d6bc07437f1f81a659
Sebastian Colon <sebastian.colon08@gmail.com>
2023-09-18 17:25:41 -0400

    done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,45 @@
 class Game {
     // code to be added
+constructor() {
+    this.startScreen = document.getElementById("game-intro")
+    this.gameScreen = document.getElementById("game-screen")
+    this.gameEndScreen =document.getElementById("game-end")
+    this.player = new Player(
+        
+    )
+    this.height = 600 //Check Que este bien.
+    this.width = 500
+    this.obstacle = []
+    this.score = 0
+    this.lives =3
+    this.gameIsOver = false //cambiar durante el proceso   
+    }
+
+start(){
+    this.gameScreen.style.height = `${this.height}px`
+    this.gameScreen.style.width = `${this.width}px`
+    this.startScreen.style.display = 'none'
+    this.gameScreen.style.display = 'inherit'
+    this.gameLoop()
+
+}
+
+gameLoop(){
+    console.log("Looping")
+
+
+    if (this.gameIsOver){
+        window.cancelAnimationFrame()
+    }
+
+    this.update()
+
+    window.requestAnimationFrame(this.gameLoop())
+}
+
+update(){
+    console.log("Looping")
+    // window.requestAnimationFrame(this.gameLoop())
+
+}
 }
--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
-  startButton.addEventListener("click", function () {
+let game;
+
+  startButton.addEventListener("click", function () {           //CLICK EVENT CUando comienza
     startGame();
   });
 
   function startGame() {
     console.log("start game");
+    game=
+    game.start()
+
+
   }
 };
